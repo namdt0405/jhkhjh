97c2d622d062f2d15b7be9fb13fbec54533511ea
You <vinhvn102@gmail.com>
2021-10-16 23:46:19 +0700

    lazy

--- jhkhjh/src/Candidates.java
+++ jhkhjh/src/Candidates.java
@@ -132,6 +132,7 @@ public class Candidates {
             }
         }
         
+        //dfvdearfeafref        
         System.out.print("email: ");
         this.email=sc.nextLine();
         System.out.print("typeCandidate: ");
